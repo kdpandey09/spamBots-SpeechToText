38fd5fa1390148f2bd6c29aa9e1ec69456edb5af
kdpandey09 <81414421+kdpandey09@users.noreply.github.com>
2021-06-15 10:53:29 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+import pyautogui as pg
+import webbrowser as wb
+import time
+m = str(input("Enter The Message You Want To Send \n"))
+t= int(input("how many times you want the message to be sent \n"))
+a = int(input("enter the time in seconds after which spam starts \n"))
+b = len(m)
+url= "https://web.whatsapp.com/"
+chrome_path="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+wb.register("chrome",None,wb.BackgroundBrowser(chrome_path))
+wb.get("chrome").open(url)
+time.sleep(a)
+spam = []
+spam = m.split()
+for p in range(t):
+     for i in spam:
+        pg.typewrite(i)
+        pg.press("enter")
