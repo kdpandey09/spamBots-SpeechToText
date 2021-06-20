de5cc4f772b6a40d33c4e55cc5e44edd1dfcdead
kdpandey09 <pandeykaustubdutt@gmail.com>
2021-06-20 13:58:43 +0530

    variable names are updated for better understanding

--- insta_spam.py
+++ insta_spam.py
@@ -2,24 +2,24 @@ import pyautogui as pg
 import webbrowser as wb
 import time
 while True:
-    m = str(input("Enter The Message You Want To Send \n"))
-    t= int(input("how many times you want the message to be sent \n"))
-    a = int(input("enter the time in seconds after which spam starts \n"))
-    b = len(m)
+    message = str(input("Enter The Message You Want To Send \n"))
+    spam_number= int(input("how many times you want the message to be sent \n"))
+    sleep_time = int(input("enter the time in seconds after which spam starts \n"))
+    b = len(message)
 
     if b > 0 :
         url= "https://www.instagram.com/direct/inbox/"
         chrome_path="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
         wb.register("chrome",None,wb.BackgroundBrowser(chrome_path))
         wb.get("chrome").open(url)
-        time.sleep(a)
+        time.sleep(sleep_time)
         spam = []
-        spam = m.split()
-        for p in range(t):
+        spam = message.split()
+        for p in range(spam_number):
              for i in spam:
                 pg.typewrite(i)
                 pg.press("enter")
         break
     else:
-        print("Don't Enter NULL Messages")
+        print("Don't Enter a Null Messages")
         continue
