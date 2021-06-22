fa195229022cdd990a814c02d0545a626ab07f2b
kdpandey09 <pandeykaustubdutt@gmail.com>
2021-06-22 19:34:01 +0530

    new one..but error hai error

---
+++ speechtotext.py
@@ -0,0 +1,10 @@
+import speech_recognition as s
+r = s.Recognizer()
+print("I am Listening you....")
+
+with s.Microphone() as source:
+    audio = r.listen(source)
+
+    message = r.recognize_google(audio,language='eng-in', show_all =True)
+
+    print(message)
